5ebee41a6e09182a2e77e70676a940b287c9e590
anna.biala94 <aniamuzyka121@wp.pl>
2018-08-16 12:28:00 +0200

    sortowanie alfabetyczne

--- public/build/js/game.js
+++ public/build/js/game.js
@@ -13,13 +13,13 @@ var Game = {
     },
     checkTeams: function () {
 
-
         var first_team = $("#game_first_team").val();
         var second_team = $("#game_second_team").val();
 
 
 
             var options = $('#game_second_team').find('option');
+            //$(options[0]).show();
             $.each(options, function (key,option) {
                // console.log($(option).val());
                 if(first_team == $(option).val()){
@@ -29,8 +29,9 @@ var Game = {
                 }
             })
 
-        var options = $('#game_first_team').find('option');
-        $.each(options, function (key,option) {
+        var options_fir = $('#game_first_team').find('option');
+        //$(options_fir[1]).show();
+        $.each(options_fir, function (key,option) {
            // console.log($(option).val());
             if(second_team == $(option).val()){
                 $(option).hide();
